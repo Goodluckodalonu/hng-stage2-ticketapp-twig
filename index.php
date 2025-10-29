eb1f6f36669aa4b50c15137011d8cfe72a3dbed1
abed-obaah <ubahobaah@gmail.com>
2025-10-29 04:25:07 +0100

    Restore Twig template rendering

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-echo "Hello from index.php";
-exit;
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+$loader = new FilesystemLoader(__DIR__ . '/templates');
+$twig = new Environment($loader);
+
+echo $twig->render('landing.twig');
